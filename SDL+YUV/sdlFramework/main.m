0d9d9d5a3e05f464b941344837f4c1247a0740f5
xiaoyuancai <18101372070@163.com>
2018-08-29 16:20:18 +0800

    add prompt

--- SDL+YUV/sdlFramework/main.m
+++ SDL+YUV/sdlFramework/main.m
@@ -5,6 +5,16 @@
 //  Created by Yuan Le on 2018/8/29.
 //  Copyright © 2018年 Yuan Le. All rights reserved.
 //
+/**
+ SDL框架应用-Mac平台？
+ 1、下载SDL2.Framework开发包
+ 地址：http://www.libsdl.org/download-2.0.php
+ 2、将SDL2.Framework拷贝到/Library/Frameworks目录下
+ 解释：在Mac安装SDL2
+ 3、在xcode下新建命令行项目，进行开发
+ 案例：播放YUV文件
+ */
+
 
 #import <Foundation/Foundation.h>
 #import <SDL2/SDL.h>
